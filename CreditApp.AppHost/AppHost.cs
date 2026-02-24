c8e701bf1668315b220550af31553aa47c418bf7
Rifinn-crypto <esh2015@list.ru>
2026-02-24 23:33:37 +0400

    :)

--- CreditApp.AppHost/AppHost.cs
+++ CreditApp.AppHost/AppHost.cs
@@ -1,8 +1,22 @@
+using Google.Protobuf.WellKnownTypes;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var redis = builder.AddRedis("redis");
 
-builder.AddProject<Projects.CreditApp_Api>("api")
-       .WithReference(redis);
+var redisCommander = builder.AddContainer("redis-commander", "rediscommander/redis-commander")
+    .WithEnvironment("REDIS_HOSTS", "local:redis:6379")
+    .WithReference(redis)
+    .WaitFor(redis)
+    .WithEndpoint(port: 8081, targetPort: 8081);
+
+var api = builder.AddProject<Projects.CreditApp_Api>("api")
+       .WithReference(redis)
+       .WaitFor(redis);
+
+builder.AddProject<Projects.Client_Wasm>("client")
+     .WithReference(api)
+     .WaitFor(api);
 
 builder.Build().Run();
+
