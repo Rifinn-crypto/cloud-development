871d7982460838b96190eb74c0e6e11539c1a488
Rifinn-crypto <esh2015@list.ru>
2026-02-24 23:20:19 +0400

    Исправления

--- CreditApp.AppHost/AppHost.cs
+++ CreditApp.AppHost/AppHost.cs
@@ -1,12 +1,8 @@
-using Microsoft.Extensions.Hosting;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-var redis = builder.AddRedis("redis")
-    .WithImage("redis:alpine")
-    .WithLifetime(ContainerLifetime.Session);
+var redis = builder.AddRedis("redis");
 
-var api = builder.AddProject<Projects.CreditApp_Api>("api")
-    .WithReference(redis);
+builder.AddProject<Projects.CreditApp_Api>("api")
+       .WithReference(redis);
 
 builder.Build().Run();
