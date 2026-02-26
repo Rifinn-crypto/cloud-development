4ec7b8f383b142e340deafa38fa528001b2f55c7
Rifinn-crypto <esh2015@list.ru>
2026-02-26 22:18:01 +0400

    Исправления с интеграцией redis, а так же дроп ненужных библиотек

--- CreditApp.AppHost/AppHost.cs
+++ CreditApp.AppHost/AppHost.cs
@@ -2,13 +2,8 @@ using Google.Protobuf.WellKnownTypes;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var redis = builder.AddRedis("redis");
-
-var redisCommander = builder.AddContainer("redis-commander", "rediscommander/redis-commander")
-    .WithEnvironment("REDIS_HOSTS", "local:redis:6379")
-    .WithReference(redis)
-    .WaitFor(redis)
-    .WithEndpoint(port: 8081, targetPort: 8081);
+var redis = builder.AddRedis("redis")
+    .WithRedisCommander();
 
 var api = builder.AddProject<Projects.CreditApp_Api>("api")
        .WithReference(redis)
